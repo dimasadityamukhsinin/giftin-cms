e2dce1c6b5e3f1d3db0f601afeff50fcd60217b3
dimasadityamukhsinin <dimasadityamukhsinin@gmail.com>
2021-04-10 12:56:21 +0700

    update

--- schemas/product.js
+++ schemas/product.js
@@ -1,66 +1,76 @@
 export default {
-  name: 'product',
-  title: 'Product',
-  type: 'document',
-  __experimental_actions: ['update', 'publish'], // disable for initial publish
+  name: "product",
+  title: "Product",
+  type: "document",
+  __experimental_actions: ["update", "publish"], // disable for initial publish
   fieldsets: [
     {
-      title: 'Shopify',
-      name: 'shopify',
-      description: 'Synced from Shopify',
-      options: { columns: 2, collapsible: true }
-    }
+      title: "Shopify",
+      name: "shopify",
+      description: "Synced from Shopify",
+      options: { columns: 2, collapsible: true },
+    },
   ],
   fields: [
     {
-      name: 'title',
-      title: 'Title',
-      type: 'string',
-      fieldset: 'shopify'
+      name: "title",
+      title: "Title",
+      type: "string",
+      fieldset: "shopify",
     },
     {
-      name: 'id',
-      title: 'ID',
-      type: 'string',
-      description: 'This comes from Shopify and cannot be changed',
+      name: "id",
+      title: "ID",
+      type: "string",
+      description: "This comes from Shopify and cannot be changed",
       readOnly: true,
-      hidden: true
+      hidden: true,
     },
     {
-      name: 'productId',
-      title: 'Product ID',
-      type: 'number',
-      description: 'This comes from Shopify and cannot be changed',
+      name: "productId",
+      title: "Product ID",
+      type: "number",
+      description: "This comes from Shopify and cannot be changed",
       readOnly: true,
-      hidden: true
+      hidden: true,
     },
     {
-      name: 'defaultPrice',
-      title: 'Default Price',
-      type: 'string',
-      description: 'This comes from Shopify and cannot be changed',
+      name: "defaultPrice",
+      title: "Default Price",
+      type: "string",
+      description: "This comes from Shopify and cannot be changed",
       readOnly: true,
-      fieldset: 'shopify'
+      fieldset: "shopify",
     },
     {
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
-      description: 'This comes from Shopify and cannot be changed',
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      description: "This comes from Shopify and cannot be changed",
       readOnly: true,
-      fieldset: 'shopify'
+      fieldset: "shopify",
     },
     {
-      name: 'productDescription',
-      title: 'Product Description',
-      type: 'blockContent'
-    }
+      name: "image",
+      title: "image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+      validation: (Rule) => Rule.required().warning("Cannot be empty"),
+    },
+    {
+      name: "productDescription",
+      title: "Product Description",
+      type: "blockContent",
+      validation: (Rule) => Rule.required().warning("Cannot be empty"),
+    },
   ],
 
   preview: {
     select: {
-      title: 'title',
-      media: 'mainImage'
-    }
-  }
-}
+      title: "title",
+      media: "mainImage",
+    },
+  },
+};
